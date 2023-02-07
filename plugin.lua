aseprite/Attachment-System

commit 3350c82884b2d008902a6c8afdf602d1bb9653e8

Allow attachment deletion from base folder only when unused

plugin.lua
@@ -788,6 +788,10 @@ local function show_tile_context_menu(ts, ti, folders, folder, indexInFolder)
     app.range.frames = frames
   end
 
+  local function is_unused_tile(tileIndex)
+    return tilesHistogram[tileIndex] == nil
+  end
+
   popup:menuItem{ text="Edit Anchors", onclick=editAnchors }:newrow()
   popup:menuItem{ text="Edit Tile", onclick=editTile }:newrow()
   popup:separator():newrow()
@@ -797,8 +801,10 @@ local function show_tile_context_menu(ts, ti, folders, folder, indexInFolder)
   popup:menuItem{ text="Select usage", onclick=selectFrames }:newrow()
   popup:menuItem{ text="Find next usage", onclick=function() find_next_attachment_usage(ti, MODE_FORWARD) end }:newrow()
   popup:menuItem{ text="Find prev usage", onclick=function() find_next_attachment_usage(ti, MODE_BACKWARDS) end }:newrow()
-  popup:separator()
-  popup:menuItem{ text="Delete", onclick=delete }
+  if folder and (not is_base_set_folder(folder) or is_unused_tile(ti)) then
+    popup:separator()
+    popup:menuItem{ text="Delete", onclick=delete }
+  end
   popup:showMenu()
   imi.repaint = true
 end
@@ -1265,6 +1271,7 @@ local function Sprite_change(ev)
   local repaint = ev.fromUndo
 
   if activeLayer and activeLayer.isTilemap then
+    tilesHistogram = calculate_tiles_histogram(activeLayer)
     local tileImg = get_active_tile_image()
     if tileImg and
        (not activeTileImageInfo or
@@ -1274,12 +1281,21 @@ local function Sprite_change(ev)
       activeTileImageInfo = { id=tileImg.id,
                               version=tileImg.version }
       shrunkenBounds = calculate_shrunken_bounds(activeLayer)
-      tilesHistogram = calculate_tiles_histogram(activeLayer)
       if not imi.isongui then
         repaint = true
       end
     else
       activeTileImageInfo = {}
+    end
+
+    local ti = get_active_tile_index()
+    if ti then
+      local folders = get_layer_properties(activeLayer).folders
+      local baseSet = get_base_set_folder(folders)
+      if not contains(baseSet.items, ti) then
+        table.insert(baseSet.items, ti)
+        activeLayer.properties(PK).folders = folders
+      end
     end
   end
 

commit b1d8cdc74e8a427feb8742a8ff0a58b0df28a251

Fix crash undoing the update of the sprite DB
Regression introduced in 3350c82884b2d008902a6c8afdf602d1bb9653e8, we
cannot update properties when we navigate the undo. We could filter
the property change using "if not ev.fromUndo then..." but in this
case we prefer to avoid modifying the Base Set if the tile is
missing (we should provide other way to add/fix missing tiles in the
Base Set, which shouldn't be a common scenario).

plugin.lua
@@ -1167,16 +1167,6 @@ local function Sprite_change(ev)
     else
       activeTileImageInfo = {}
     end
-
-    local ti = get_active_tile_index()
-    if ti then
-      local folders = db.getLayerProperties(activeLayer).folders
-      local baseSet = db.getBaseSetFolder(activeLayer, folders)
-      if not contains(baseSet.items, ti) then
-        table.insert(baseSet.items, ti)
-        activeLayer.properties(PK).folders = folders
-      end
-    end
   end
 
   if repaint then
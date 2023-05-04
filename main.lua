23acff94070d07f7e15b8a35521f3fef7dfb59a2
Gaspar Capello <gaspar@igara.com>
2023-05-04 17:34:17 -0300

    Add infinite loop prevention in main.alignAnchors when there is an accidental hierarchy loop.

--- main.lua
+++ main.lua
@@ -275,45 +275,46 @@ local function set_ref_point(ts, ti, point)
   ts:tile(ti).properties(PK).ref = point
 end
 
--- Matches defined reference points <-> anchor points from parent to
--- children
-function main.alignAnchors()
-  local spr = app.activeSprite
-  if not spr then return end
-
-  local hierarchy = {}
-  local function create_layers_hierarchy(layers)
-    for i=1,#layers do
-      local layer = layers[i]
-      if layer.isTilemap then
-        local layerProperties = layer.properties(PK)
-        if layerProperties.id then
-          local ts = get_base_tileset(layer)
-          for ti=1,#ts-1 do
-            local anchors = ts:tile(ti).properties(PK).anchors
-            if anchors then
-              for j=1,#anchors do
-                local auxLayer = find_layer_by_id(spr.layers, anchors[j].layerId)
-                if auxLayer then
-                  local childId = anchors[j].layerId
-                  if childId then
-                    hierarchy[childId] = layerProperties.id
-                  end
+local function create_layers_hierarchy(layers, hierarchy)
+  for i=1,#layers do
+    local layer = layers[i]
+    if layer.isTilemap then
+      local layerProperties = layer.properties(PK)
+      if layerProperties.id then
+        local ts = get_base_tileset(layer)
+        for ti=1,#ts-1 do
+          local anchors = ts:tile(ti).properties(PK).anchors
+          if anchors then
+            for j=1,#anchors do
+              local auxLayer = find_layer_by_id(layers, anchors[j].layerId)
+              if auxLayer then
+                local childId = anchors[j].layerId
+                if childId then
+                  hierarchy[childId] = layerProperties.id
                 end
               end
             end
           end
         end
       end
-      if layer.isGroup then
-        create_layers_hierarchy(layer.layers)
-      end
+    end
+    if layer.isGroup then
+      create_layers_hierarchy(layer.layers)
     end
   end
-  create_layers_hierarchy(spr.layers)
+end
+
+-- Matches defined reference points <-> anchor points from parent to
+-- children
+function main.alignAnchors()
+  local spr = app.sprite
+  if not spr then return end
+
+  local hierarchy = {}
+  create_layers_hierarchy(spr.layers, hierarchy)
 
   local movedLayers = {}
-  local function align_layer(childId, parentId, tab)
+  local function align_layer(childId, parentId, tab, hierarchy_control)
     local child = find_layer_by_id(spr.layers, childId)
     local parent = find_layer_by_id(spr.layers, parentId)
 
@@ -321,7 +322,19 @@ function main.alignAnchors()
     assert(parent)
 
     if hierarchy[parentId] then
-      align_layer(parentId, hierarchy[parentId], tab+1)
+      for i=1, #hierarchy_control, 1 do
+        if hierarchy[parentId] == hierarchy_control[i] then
+          app.alert("Hierarchy loop error. Try to fix Parent>Child chain of the next layers: " ..
+                    find_layer_by_id(spr.layers, hierarchy_control[i]).name ..
+                    " & " ..
+                    find_layer_by_id(spr.layers, parentId).name)
+          return false
+        end
+      end
+      table.insert(hierarchy_control, parentId)
+      if not align_layer(parentId, hierarchy[parentId], tab+1, hierarchy_control) then
+        return false
+      end
     end
 
     if not movedLayers[childId] then
@@ -350,11 +363,15 @@ function main.alignAnchors()
         end
       end
     end
+    return true
   end
 
   app.transaction("Align Anchors", function()
     for childId,parentId in pairs(hierarchy) do
-      align_layer(childId, parentId, 0)
+      local hierarchy_control = { childId }
+      if not align_layer(childId, parentId, 0, hierarchy_control) then
+        break
+      end
     end
     app.refresh()
   end)
